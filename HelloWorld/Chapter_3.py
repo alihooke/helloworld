fb0ecdd280f13cfb88b836f7c056d12dcbc1ae9f
alihooke <alihooke@gmail.com>
2014-04-03 22:09:51 -0700

    Chapter 3 Challenge 1

---
+++ HelloWorld/Chapter_3.py
@@ -0,0 +1,20 @@
+__author__ = 'alexishooke'
+
+def challenge_1():
+
+    import random
+    fortune = random.randint(1,5)
+    if fortune == 1:
+        print("Something you lost will soon turn up.")
+    if fortune == 2:
+        print("You will be invited to an exciting event.")
+    if fortune == 3:
+        print("The one you love is closer than you think.")
+    if fortune == 4:
+        print("Sun always shines after a downpour.")
+    if fortune == 5:
+        print("Idleness is the holiday of fools.")
+
+def challenge_2():
+    
+

0fa667a82971695ff611214cda7f1bf836bb19a2
alihooke <alihooke@gmail.com>
2014-04-07 20:14:05 -0700

    Chapter 3, challenge 2

--- HelloWorld/Chapter_3.py
+++ HelloWorld/Chapter_3.py
@@ -15,14 +15,6 @@ def challenge_1():
     if fortune == 5:
         print("Idleness is the holiday of fools.")
 
-def challenge_2():
-    import random
-    coinflip = random.randint(1, 2)
-    if coinflip() == 1:
-        print("heads")
-    if coinflip() == 2:
-        print("tails")
-
 
 def challenge_1_2():
     import random
@@ -30,8 +22,26 @@ def challenge_1_2():
     fortune = random.choice(fortunelist)
     print(fortune)
 
-challenge_1_2()
 
+def challenge_2():
+    import random
+    heads = 0
+    tails = 0
+    count = 0
 
+    flips = int(input("How many times shall I flip a coin?"))
+    while flips > count:
+        coinflip = random.randint(0,1)
+        if coinflip == 0:
+            count = count + 1
+            heads = heads + 1
+        elif coinflip == 1:
+            count = count + 1
+            tails = tails + 1
+        else:
+            print("error")
+    print("There were", heads, "heads and", tails, "tails.")
+
+challenge_2()
 
 
